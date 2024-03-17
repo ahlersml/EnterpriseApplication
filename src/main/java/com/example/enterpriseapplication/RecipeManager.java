4d50fadb26915530189812e31c4a8a1d08192c8c
patel6nr <patel6nr@mail.uc.edu>
2024-03-17 17:14:05 -0400

    Refactor RecipeManager class to improve code readability and add new features

--- src/main/java/com/example/enterpriseapplication/RecipeManager.java
+++ src/main/java/com/example/enterpriseapplication/RecipeManager.java
@@ -5,19 +5,23 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class RecipeManager {
     private static final String JSON_FILE = "recipes.json";
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+    // Read recipes from JSON file
     private static List<Recipe> readRecipesFromJson() {
-        try (Reader reader = new FileReader(JSON_FILE)) {
-            Type listType = new TypeToken<ArrayList<Recipe>>() {
-            }.getType();
+        try (FileReader reader = new FileReader(JSON_FILE)) {
+            Type listType = new TypeToken<ArrayList<Recipe>>() {}.getType();
             return gson.fromJson(reader, listType);
         } catch (IOException e) {
             e.printStackTrace();
@@ -25,12 +29,7 @@ public class RecipeManager {
         }
     }
 
-    private static void displayRecipes(List<Recipe> recipes) {
-        for (Recipe recipe : recipes) {
-            System.out.println(recipe);
-        }
-    }
-
+    // Save recipes to JSON file
     private static void saveRecipesToJson(List<Recipe> recipes) {
         try (Writer writer = new FileWriter(JSON_FILE)) {
             gson.toJson(recipes, writer);
@@ -39,12 +38,10 @@ public class RecipeManager {
         }
     }
 
-    private static Recipe findRecipeByName(List<Recipe> recipes, String name) {
-        for (Recipe recipe : recipes) {
-            if (recipe.getRecipeName().equals(name)) {
-                return recipe;
-            }
-        }
-        return null;
+    // Find recipe by name
+    private static Optional<Recipe> findRecipeByName(List<Recipe> recipes, String name) {
+        return recipes.stream()
+                .filter(recipe -> recipe.getRecipeName().equals(name))
+                .findFirst();
     }
 }
