3ea45bed583d5a8d527d0c39876b7ada24f6f984
ahlersml <70450921+ahlersml@users.noreply.github.com>
2024-04-10 22:10:14 -0400

    Revert "Final version"
    This reverts commit 3c83c5fbcc5cf4e654b1e89cdf7f1e86e62f6158.

--- src/main/resources/static/RecipeManager.js
+++ src/main/resources/static/RecipeManager.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
         headers: myHeaders,
         redirect: "follow"
     };
-    fetch("https://sweepster-api.onrender.com/api/meals", requestOptions)
+    fetch("http://localhost:8181/api/meals", requestOptions)
         .then((response) => response.json())
         .then((result) => {
             console.log(result);
